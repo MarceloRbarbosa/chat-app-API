063239a9c82701b48cf29035e24a6b6652da8bb5
Marcelo R. Barbosa <marceloribeirobarbosa92@gmail.com>
2026-02-05 18:22:52 -0300

    feat: add JWT && JWT_sECRET

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,6 @@
 import usersRepository from "../repositories/userRepository";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 async function postNewUser(username:string, password: string){
     const conflict = await usersRepository.findUserByUsername(username)
@@ -13,9 +14,24 @@ async function postNewUser(username:string, password: string){
         return newUser;
     }
 
+async function findUsers(username:string, password: string){   
+    
+    const user = await usersRepository.findUserByUsername(username);    
+    if(!user) throw { type: "NOT_FOUND", message: "User not found"}
+    
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if(!passwordMatch) throw { type: "unauthorized", message:"Invalid Password"}
+
+    const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET as string,
+        {expiresIn: "30"})
+
+    return token
+}
 
 const userService = {
-    postNewUser
+    postNewUser, findUsers
 }
 
 export default userService;
